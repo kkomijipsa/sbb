7637a0198465ab80a1394c8cf06e84d4ba836ba4
RSH <fbtnghks7906@gmail.com>
2024-01-04 20:54:35 +0900

    [test] Question 엔티티 제목 데이터 수정 테스트 코드 작성

--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 import java.util.Queue;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -20,7 +21,7 @@ class SbbApplicationTests {
 
   @Test
   void testJpa() {
-//    데이터 저장
+//    질문 데이터 저장
 //    Question q1 = new Question();
 //    q1.setSubject("sbb가 뭐임?");
 //    q1.setContent("sbb에 대해 알고싶음");
@@ -32,7 +33,8 @@ class SbbApplicationTests {
 //    q2.setContent("id는 자동으로 생성되나요?");
 //    q2.setCreateDate(LocalDateTime.now());
 //    this.questionRepository.save(q2); // 두번째 질문 저장
-
+//-------------------------------------------------------
+//    질문 데이터 조회
 //    질문 갯수 조회와 제목과 같은지 테스트
 //    List<Question> all = this.questionRepository.findAll();
 //    assertEquals(2, all.size());
@@ -56,8 +58,22 @@ class SbbApplicationTests {
 //      assertEquals(1, q.getId());
 
 //    제목에 특정 문자열이 포함되어 있는 데이터 조회
-    List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
-    Question q = qList.get(0);
-    assertEquals("sbb가 뭐임?", q.getSubject());
+    /**
+     * sbb%: "sbb"로 시작하는 문자열
+     * %sbb: "sbb"로 끝나는 문자열
+     * %sbb%: "sbb"를 포함하는 문자열
+     */
+
+//    List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+//    Question q = qList.get(0);
+//    assertEquals("sbb가 뭐임?", q.getSubject());
+
+//    -------------------------------------------------------
+//    질문 데이터 수정
+    Optional<Question> oq = this.questionRepository.findById(2);
+    assertTrue(oq.isPresent());
+    Question q = oq.get();
+    q.setSubject("수정된 제목");
+    this.questionRepository.save(q);
   }
 }
