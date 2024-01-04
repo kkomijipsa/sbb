0abfb5cb49a44fa250aeb84bb728e4c4aa120307
RSH <fbtnghks7906@gmail.com>
2024-01-04 21:43:13 +0900

    [fix] 테스트 코드 에러 수정

--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -51,16 +51,16 @@ class SbbApplicationTests {
 //    id값으로 데이터 조회
     Optional<Question> oq = this.questionRepository.findById(1);
     if (oq.isPresent()) {
-      Question q = oq.get();
+      q = oq.get();
       assertEquals("sbb가 뭐임?", q.getSubject());
     }
 
 //    제목으로 데이터 조회
-    Question q = this.questionRepository.findBySubject("sbb가 뭐임?");
+    q = this.questionRepository.findBySubject("sbb가 뭐임?");
     assertEquals(1, q.getId());
 
 //    제목과 내용으로 데이터 조회
-    Question q = this.questionRepository.findBySubjectAndContent("sbb가 뭐임?", "sbb에 대해 알고싶음");
+    q = this.questionRepository.findBySubjectAndContent("sbb가 뭐임?", "sbb에 대해 알고싶음");
     assertEquals(1, q.getId());
 
 //    제목에 특정 문자열이 포함되어 있는 데이터 조회
@@ -71,31 +71,31 @@ class SbbApplicationTests {
      */
 
     List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
-    Question q = qList.get(0);
+    q = qList.get(0);
     assertEquals("sbb가 뭐임?", q.getSubject());
 
 //    -------------------------------------------------------
 //    질문 데이터 수정
-    Optional<Question> oq = this.questionRepository.findById(2);
+    oq = this.questionRepository.findById(2);
     assertTrue(oq.isPresent());
-    Question q = oq.get();
+    q = oq.get();
     q.setSubject("수정된 제목");
     this.questionRepository.save(q);
 
 //    -------------------------------------------------------
 //    질문 데이터 삭제
     assertEquals(2, this.questionRepository.count());
-    Optional<Question> oq = this.questionRepository.findById(1);
+    oq = this.questionRepository.findById(1);
     assertTrue(oq.isPresent());
-    Question q = oq.get();
+    q = oq.get();
     this.questionRepository.delete(q);
     assertEquals(1, this.questionRepository.count());
 
 //    -------------------------------------------------------
 //    답변 데이터 생성 후 저장하기
-    Optional<Question> oq = this.questionRepository.findById(2);
+    oq = this.questionRepository.findById(2);
     assertTrue(oq.isPresent());
-    Question q = oq.get();
+    q = oq.get();
 
     Answer a = new Answer();
     a.setContent("네 자동으로 생성됩니다.");
@@ -107,14 +107,14 @@ class SbbApplicationTests {
 //    답변 조회(id가 1인 답변을 조회 후 그 답변의 질문 id가 2인것도 조회 = 답변에 연결된 질문 찾기)
     Optional<Answer> oa = this.answerRepository.findById(1);
     assertTrue(oa.isPresent());
-    Answer a = oa.get();
+    a = oa.get();
     assertEquals(2, a.getQuestion().getId());
 
 //    -------------------------------------------------------
 //    답변 조회(질문에 연결된 답변 찾기)
-    Optional<Question> oq = this.questionRepository.findById(2);
+    oq = this.questionRepository.findById(2);
     assertTrue(oq.isPresent());
-    Question q = oq.get();
+    q = oq.get();
 
     List<Answer> answerList = q.getAnswerList();
 
